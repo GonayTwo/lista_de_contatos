1fb3e5bd9afc7181e83e3021e1dded552cf6064b
Daniel Miranda <daniel_miranda97@hotmail.com>
2021-11-10 21:13:07 -0300

    Commit 4

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -25,7 +25,7 @@ class ContactHelper{
   }
   Future<Database>initDb() async{
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
 
     return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async{
       await db.execute(
@@ -38,15 +38,41 @@ class ContactHelper{
     contact.id = await dbContact.insert(contactTable, contact.toMap());
     return contact;
   }
+  Future<int>deleteContact(int id) async{
+    Database dbContact = await db;
+    return await dbContact.delete(contactTable, where: "$idColumn = ?",whereArgs: [id]);
+
+  }
+  Future<int>updateContact(Contact contact) async{
+    Database dbContact = await db;
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?",whereArgs: [contact.id]);
+  }
   Future<Contact> getContact(int id) async {
     Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,columns: [idColumn,nameColumn,emailColumn,phoneColumn,imgColumn],
     where: "$idColumn = ?",whereArgs: [id]);
     if(maps.length >0){
       return Contact.fromMap(maps.first);
-    }else{
+    }else {
       return null;
     }
+  }
+  Future<List>getAllContacts() async{
+    Database dbContact = await db;
+    List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
+    List<Contact> listContact = List();
+    for(Map m in listMap){
+      listContact.add(Contact.fromMap(m));
+    }
+    return listContact;
+  }
+  Future<int>getNumber()async{
+    Database dbContact = await db;
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }
+  Future close()async{
+    Database dbContact = await db;
+    dbContact.close();
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lista_de_contatos/ui/home_page.dart';
 
 main(){
-  runApp(MyApp());
+  runApp(HomePage());
 }
 
-class MyApp(){
 
-}
